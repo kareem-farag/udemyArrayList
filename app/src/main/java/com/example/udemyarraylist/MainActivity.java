4cabb60e9519bbf9da78d4d897cb1407bd3f7ccb
Kareem Farag <kareemfarag@hotmail.com>
2020-04-05 23:41:54 +0200

    commit #2 : Adding ArrayAdapter to the GridView

--- app/src/main/java/com/example/udemyarraylist/MainActivity.java
+++ app/src/main/java/com/example/udemyarraylist/MainActivity.java
@@ -2,6 +2,15 @@ package com.example.udemyarraylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> myarrayList = new ArrayList(asList("Kareem","Ahmed","Khaled","Mohamed","Kinzy"));
+        GridView gridView = findViewById(R.id.gridView);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,myarrayList);
+        gridView.setAdapter(arrayAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"sada",Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
